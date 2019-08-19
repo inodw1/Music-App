9c4fd6b86714943aa9558e2f882fa99045485ecd
Inod Wagachchi <inod@tsdclanka.com>
2019-08-19 08:57:23 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+    <app-home></app-home>
+  </div>
+</template>
+
+<script>
+
+import Home from './components/Home'
+export default {
+  components: {
+    'app-home': Home
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <form>
+        <fieldset>
+          <legend>Music App</legend>
+          <div class="form-group row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Search Music</label>
+            <div class="col-sm-10">
+              <input
+                type="text"
+                readonly
+                class="form-control-plaintext"
+                id="staticEmail"
+                value="email@example.com"
+              />
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+    <div class="col-md-12">
+        Results
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
